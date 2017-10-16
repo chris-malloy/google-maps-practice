56f857f595572a0f3dafdeaf3bc896da220b631a
chris-malloy <chrismalloymusic@gmail.com>
2017-10-16 14:45:51 -0400

    working with zoom and directions

--- scripts.js
+++ scripts.js
@@ -1,55 +1,74 @@
-console.log(cities)
-
-// this functions like document.ready()
-function initMap() {
-    console.log("Map loaded")
+$(document).ready(function() {
+    var google = window.google;
+    // console.log('map loaded');
+    // geographic center of the US
     myLatlng = {
         lat: 40.0000,
         lng: -98.0000
     };
+    // Init the map to load at geoCenter, zoom 4
     var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: myLatlng
-        })
-        // markers array
+        zoom: 4,
+        center: myLatlng
+    });
+    // markers array
     var markers = [];
     // global infoWindow for everyone to share
     var infoWindow = new google.maps.InfoWindow({});
-    // loop through the cities aray which is in cities.js
+    // loop through the cities array which is in cities.js
     var listHTML = '';
     cities.map((city) => {
         createMarker(city);
         listHTML += addCityToList(city);
     });
     $('#cities-table tbody').html(listHTML);
+    $('.city-name').click(function() {
+        // trigger will simulate any google maps event
+        // 2 args what element and which event
+        var index = $(this).attr('index');
+        google.maps.event.trigger(markers[index], "click");
+    });
+    $('.city-zoom').click(function() {
+        console.log($(this))
+        var index = $(this).attr('index');
+        // console.log(cities)
+        zoomToCity(cities[index].lat, cities[index].lon)
+    });
+    $('.city-directions').click(function() {
+        var index = $(this).attr('index')
+    })
+
+    // Add submit listener to the form
     $('#filter-form').submit(function(event) {
-        // wipe out all markers
-        // so that you can view the only the markers that the user wants
-        markers.map(function(marker) {
+        // wipe out all the markers
+        markers.map((marker) => {
             marker.setMap(null);
         });
+
+
         event.preventDefault();
-        console.log("User submission");
-        // set our user input to lower case
+        // user submitted the input box
+        // console.log("User submission!");
         var userSearch = $('#filter-input').val().toLowerCase();
         listHTML = '';
         cities.map((city) => {
-            // set all city names to lower case in our cities object
             var cityName = city.city.toLowerCase();
-            if (city.city.indexOf(userSearch) !== -1) {
-                // the city we are on, contains the search text the user entered
+            if (cityName.indexOf(userSearch) > -1) {
+                // The city we are on, contains the search text the user entered
                 createMarker(city);
                 listHTML += addCityToList(city);
             }
         });
+        $('#cities-table tbody').html(listHTML);
+
     });
 
-    function addCityToList(city) {
+    function addCityToList(city, index) {
         var newHTML = '<tr>';
-        newHTML += `<td class="city-name">${city.city}</td>`;
+        newHTML += `<td class="city-name" index=${city.yearRank - 1}>${city.city}</td>`;
         newHTML += `<td class="city-state">${city.state}</td>`;
         newHTML += `<td class="city-directions"><button class="btn btn-primary">Get Directions</button></td>`;
-        newHTML += `<td class="city-zoom"><button class="btn btn-success">Zoom</button></td>`;
+        newHTML += `<td class="city-zoom"><button class="btn btn-success">Zoom to city</button></td>`;
         newHTML += '</tr>'
         return newHTML;
     }
@@ -58,10 +77,12 @@ function initMap() {
         // console.log(city);
         // set up an object with this cities lat/lon
         var cityLL = {
-            lat: city.lat,
-            lng: city.lon
-        }
+                lat: city.lat,
+                lng: city.lon
+            }
+            // set up a marker for current city
         var marker = new google.maps.Marker({
+            // WHERE
             position: cityLL,
             map: map,
             title: city.city
@@ -70,17 +91,46 @@ function initMap() {
         // addListener takes 3 args:
         // 1. What
         // 2. Event
-        // 3. Code to run
+        // 3. Code to run...
         google.maps.event.addListener(marker, 'click', () => {
-
+            // all infoWindows (becuase they are constructors), have a setContent
+            // method which is like .html("blah blah blah")
             var infoWindowHTML = `<h2>${city.city}</h2>`
             infoWindowHTML += `<h4>City population: ${city.yearEstimate}</h4>`
-                // open takes 2 args:
-                // 1. Map to open to infoWindow on
-                // 2. Where to put the infoWindow on the map
+            infoWindowHTML += '<img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Wonder_Woman_%28DC_Rebirth%29.jpg/170px-Wonder_Woman_%28DC_Rebirth%29.jpg">';
             infoWindow.setContent(infoWindowHTML);
+            // open takes 2 args:
+            // 1. Map to open the infoWindow on
+            // 2. Where to put the infoWindow on teh map
             infoWindow.open(map, marker);
         })
         markers.push(marker);
     }
-};
+    // Lat/lon will correspond to one of our cities
+    function zoomToCity(lat, lon) {
+        // google maps api has a constructor to make a latlng object
+        var LL = new google.maps.LatLng(lat, lon);
+        console.log(LL);
+        console.log(lat, lon);
+        map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 15,
+                center: LL
+            }
+        );
+        infoWindow = new google.maps.InfoWindow();
+        var places = new google.maps.places.PlacesService(map);
+        // places has a method called "nearbySearch" that we pas an object with 
+        // 1. location
+        // 2. radius
+        // 3. type
+        places.nearbySearch({
+            location: LL,
+            radius: 500,
+            type: ['stadium']
+
+        }, function(results, status) {
+            console.log(results);
+        });
+    }
+});
